87f1e0ccd0371eb00d7a10b83dc37aca3a9810e8
kacpermejs <kacpermejs@gmail.com>
2025-07-21 12:24:41 +0200

    Project rename

--- Assets/_Scripts/RuleTiles/PlatformRuleTile.cs
+++ Assets/_Scripts/RuleTiles/PlatformRuleTile.cs
@@ -5,11 +5,8 @@ using UnityEngine.Tilemaps;
 
 [CreateAssetMenu]
 public class PlatformRuleTile : RuleTile<PlatformRuleTile.Neighbor> {
-    public bool customField;
 
     public class Neighbor : RuleTile.TilingRule.Neighbor {
-        public const int Null = 3;
-        public const int NotNull = 4;
     }
 
     public override bool RuleMatch(int neighbor, TileBase tile) {
