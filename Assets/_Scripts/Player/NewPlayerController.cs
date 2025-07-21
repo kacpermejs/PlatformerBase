2e6537bc473e36cb152439b3d9939530fbcea90e
kacpermejs <kacpermejs@gmail.com>
2025-07-21 14:13:38 +0200

    Player Controller & updated Tilemap

---
+++ Assets/_Scripts/Player/NewPlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using TarodevController;
+using UnityEngine.InputSystem;
+
+namespace Assets._Scripts.Player {
+    public class NewPlayerController : PlayerController {
+        protected InputAction m_MoveAction;
+        protected InputAction m_JumpAction;
+
+        protected override void Awake() {
+            m_MoveAction = InputSystem.actions.FindAction("Move");
+            m_JumpAction = InputSystem.actions.FindAction("Jump");
+            base.Awake();
+        }
+
+        //old
+        protected override FrameInput GatherFrameInput() {
+            Vector2 move = m_MoveAction.ReadValue<Vector2>();
+
+            bool jumpPressed = m_JumpAction.WasPressedThisFrame();
+            bool jumpHeld = m_JumpAction.IsPressed();
+
+            return new FrameInput {
+                JumpDown = jumpPressed,
+                JumpHeld = jumpHeld,
+                Move = move
+            };
+        }
+    }
+}
